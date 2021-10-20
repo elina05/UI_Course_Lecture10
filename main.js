9c8007e05a52151134bdca8cda37674154e1cb60
Elina <elinatymtsev@gmail.com>
2021-10-20 11:31:06 +0300

    10lesson

---
+++ main.js
@@ -0,0 +1,11 @@
+let myName = "Elina ";
+let age = 16;
+let question = " and I like learn Front-end";
+
+
+let click =document.getElementById('click');
+
+click.onclick=function () {
+    alert(myName + age + question)
+};
+ 
